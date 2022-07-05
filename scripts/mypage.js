a72eca356f2bf4db532aa27e0eed8b99cfb66ac3
mingoodjob <psjlmk@gmail.com>
2022-07-05 15:54:22 +0900

    fix mypage js

--- scripts/mypage.js
+++ scripts/mypage.js
@@ -1,48 +1,53 @@
-BACK_URL = "http://localhost:8000/";
+BACK_URL = "http://127.0.0.1:8000/";
 
 async function mypage_get() {
+
     const response = await fetch(BACK_URL + "post/collection/", {
-        method: "GET",
         headers: {
             "Content-Type": "application/json",
-            "Authorization": "Bearer " + localStorage.getItem("yujeon_access_token"),
+            Authorization: "Bearer " + localStorage.getItem("yujeon_access_token"),
         },
-
+        method: "GET",
     });
-
-    const data = await response.json();
-
+    const response_json = await response.json();
     if (response.status === 200) {
-        // console.log(data)
-        data.forEach(item => {
-            // let like = item.post.like
 
-            let image = item.post.artimage
-            console.log(image)
 
-            let html = `<ul><li>
-        <img src="${image}" />
-        <div class="my_picture_cost">
-            50P
-        </div>
-        <div class="my_picture_like">
-            좋아요 5개
-        </div>
-        <div class="my_picture_public">
-            지금 공개
-        </div>
-    </li></ul>`
+        response_json.forEach(element => {
+            console.log(element.post.artimage)
+            let image = element.post.artimage
+
+            let html = `<li>
+            <img src="${image}" />
+            <div class="my_picture_cost">
+                50P
+            </div>
+            <div class="my_picture_like">
+                좋아요 5개
+            </div>
+            <div class="my_picture_public">
+                지금 공개
+            </div>
+        </li>`
 
             document.getElementById("picture_list_get").innerHTML += html
-
-
         });
-    }
-    else if (response === 401) {
-        console.log("너 인증안됨 ㅅㄱ")
-    }
-    else if (response === 500) {
-        console.log("서버 터짐!")
+
+        var options = {
+            start: 0,
+            fadeIn: 400,
+            loop: true,
+            buttonPrev: 'Previous',
+            buttonNext: 'Next',
+            style: 'carousel',
+            spacing: -0.3,
+            nav: false,
+            buttons: false
+        };
+
+        $('.my_picture_list').flipster(options);
+
+
     }
 
 }
